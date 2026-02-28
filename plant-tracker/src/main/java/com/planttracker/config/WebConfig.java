a773dc55993fe6ac7db1a0d5c496edbd802b63b1
EvaWilson67 <143644462+EvaWilson67@users.noreply.github.com>
2026-02-27 23:16:25 -0500

    Needing to update where the client is being run at, and also allowing for the unique link to have access for CORS

--- plant-tracker/src/main/java/com/planttracker/config/WebConfig.java
+++ plant-tracker/src/main/java/com/planttracker/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://funny-liger-c00f87.netlify.app", "http://localhost:5173")
+                        .allowedOrigins("https://funny-liger-c00f87.netlify.app", "http://localhost:5173", "https://csce548.onrender.com/", "https://69a26a241c8f10f797f0d67a--funny-liger-c00f87.netlify.app/")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowCredentials(true);
             }
